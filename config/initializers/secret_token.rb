0af7de5eb0388d61bc8e3c92f133bdb82d116417
James Fisher <fisher14@gmail.com>
2012-06-13 17:08:28 -0400

    initial commit, base rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fisherific::Application.config.secret_token = '840728b490a46d77865606817b542d44e718dfda92099b26fa8750d4505f9e7436f45f4f683da538bbfea6200ca48aa69d0716b002df3f6dc4b8f2451021cec5'

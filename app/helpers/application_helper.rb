983e5357bfba0db45320a33253686c36afc98feb
James Fisher <fisher14@gmail.com>
2012-06-14 12:45:03 -0400

    added some helpers, better railsy links, smart header, 1 new project entry"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  
+  def full_title(page_title)
+    base_title = "fisherific"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+  
+  def menu_select(title)
+    if title == controller.action_name
+      "class='selected'"
+    end
+  end
+
 end

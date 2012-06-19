68f98e08a75f61e603cf297b5dcde95e6635ea8d
James Fisher <james@fisherific.com>
2012-06-18 20:16:29 -0400

    changed to fisherific.com (linode) with cap deployment.  Also new 404 page.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,14 +5,14 @@ set :repository,  "https://github.com/fisherific/fisherific.git"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "fisherific.net"                          # Your HTTP server, Apache/etc
-role :app, "fisherific.net"                          # This may be the same as your `Web` server
+role :web, "fisherific.com"                          # Your HTTP server, Apache/etc
+role :app, "fisherific.com"                          # This may be the same as your `Web` server
 #role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
 set :user, 'deploy'
 set :use_sudo, false
-set :deploy_to, "/var/www/#{application}'
+set :deploy_to, "/var/www/#{application}"
 set :deploy_via, :remote_cache
 
 # if you want to clean up old releases on each deploy uncomment this:
